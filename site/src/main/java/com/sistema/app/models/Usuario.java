35a8507a015af3314e974e3cd4da52d277190f08
Randazzo649 <riccardo.radazzo.2018@gmail.com>
2026-08-18 14:54:35 -0300

    Ambiente inicial para desenvolvimento do projeto.

---
+++ site/src/main/java/com/sistema/app/models/Usuario.java
@@ -0,0 +1,30 @@
+package com.sistema.app.models;
+
+public class Usuario {
+    private long id;
+    private String email;
+    private String senha;
+    public Usuario(){
+        this.id = 0;
+        this.email = "";
+        this.senha = "";
+    }
+    public void setId(long id){
+        this.id = id;
+    }
+    public long getId(){
+        return this.id;
+    }
+    public void setEmail(String email){
+        this.email = email;
+    }
+    public String getEmail(){
+        return this.email;
+    }
+    public void setSenha(String senha){
+        this.senha = senha;
+    }
+    public String getSenha(){
+        return this.senha;
+    }
+}
