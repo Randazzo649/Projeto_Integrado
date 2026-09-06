05ba68c483263be6634018f0091e3922180f14b0
PedroHKV <piphkv@gmail.com>
2026-09-06 02:25:03 -0300

    implementacao da interface de login

---
+++ site/src/main/resources/static/js/solicitacao_cadastro.js
@@ -0,0 +1,13 @@
+const cnpj_in = document.getElementById("cnpj_in");
+const razao_social_in = document.getElementById("razao");
+const nome_in = document.getElementById("nome_fantasia_in");
+const telefone_in = document.getElementById("telefone_in");
+const endereco_in = document.getElementById("endereco_in");
+const email_in = document.getElementById("email_in");
+const senha_in = document.getElementById("senha_in");
+const file_in = document.getElementById("file_in");
+const file_in_btn = document.getElementById("file_in_btn");
+const add_funcs_btn = document.getElementById("add_funcs_btn");
+const salvar_btn = document.getElementById("salvar_btn");
+
+
